e3a23f1cdd67ed669fee3ca9ffc5dbf050547353
sachin <sachin.mahto@functionup.org>
2023-12-28 15:36:03 +0530

    feat: book library

---
+++ src/book.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const sampleBookDetails =  {
+  "author": "Louis-Ferdinand Céline",
+  "country": "France",
+  "imageLink": "images/voyage-au-bout-de-la-nuit.jpg",
+  "language": "French",
+  "link": "https://en.wikipedia.org/wiki/Journey_to_the_End_of_the_Night\n",
+  "pages": 505,
+  "title": "Journey to the End of the Night",
+  "year": 1932
+}
+
+function Book() {
+  const [searchParams] = useSearchParams();
+  const [bookDetails, setBookDetails] = useState({})
+  const title = searchParams.get('title')
+  async function getBookDeatails(){
+    //make GET - “/book/details?title=X” api call here to get details of the book.
+    setBookDetails(sampleBookDetails) // replace this sample data with response data you are getting from api call
+  }
+
+  useEffect(() => { 
+    getBookDeatails()
+  }, [])
+  
+  return (
+    <main className="book-details-container">
+      <div id="book-card" className="book-details">
+        <p><span>Title:</span> {bookDetails.title}</p>
+        <p><span>Author:</span> {bookDetails.author}</p>
+        <p><span>Year:</span> {bookDetails.year}</p>
+        <p><span>country:</span> {bookDetails.country}</p>
+        <p><span>pages:</span> {bookDetails.pages}</p>
+        <p><span>language:</span> {bookDetails.language}</p>
+      </div>
+    </main>
+  );
+}
+
+export default Book;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Book from './book';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+          <Route index element={<App />} />
+          <Route path="book" element={<Book />} />
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
